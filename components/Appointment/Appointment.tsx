e85eb0ac75cdaa51caa91cfd2aed899bf9baba9f
Noah Truman <noah.truman@digital.mod.uk>
2025-08-08 09:21:27 +0100

    style Appointment

--- components/Appointment/Appointment.tsx
+++ components/Appointment/Appointment.tsx
@@ -1,3 +1,5 @@
+import styles from './Appointment.module.css';
+
 export type AppointmentProps = { 
     id: number;
     date: string; 
@@ -7,7 +9,7 @@ export type AppointmentProps = {
 
 export default function Appointment ({ appointment }: { appointment: AppointmentProps }) {
     return (
-        <div>
+        <div className={styles.appointment}>
             <h3>{appointment.description}</h3>
             <p>{appointment.time}</p>
         </div>
--- components/Day/Day.tsx
+++ components/Day/Day.tsx
@@ -1,22 +1,22 @@
 import type { Day } from "@/lib/getOrderedDays";
 import Appointment, { AppointmentProps } from "@/components/Appointment/Appointment";
 import Link from "next/link";
-import styles from "./DayDisplay.module.css";
+import styles from "./Day.module.css";
 
 export default function Day({ day, appointments }:  { day: Day , appointments: Array<AppointmentProps> }) {
     
     const href = `/day_view/${day.dayString}`;
     return (
-        <Link href={href}>
-            <div className={styles["day-display"]}>
+        <div className={styles["day-display"]}>
+            <Link href={href}>
                 <div className={styles["date-display"]}>
                     <h2>{day.dayString}</h2>
                     <h3>{day.day} {day.monthString} {day.year}</h3>
                 </div>
-                <div>
-                    {appointments && appointments.map(appointment => <Appointment key={appointment.id} appointment={appointment} />)}
-                </div>
+            </Link>
+            <div>
+                {appointments && appointments.map(appointment => <Appointment key={appointment.id} appointment={appointment} />)}
             </div>
-        </Link>
+        </div>
     );
 }
